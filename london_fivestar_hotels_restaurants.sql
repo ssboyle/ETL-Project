17541c5de2ed60c911ac38a95824802eb4f03f1b
ssboyle <sarah.sprole@gmail.com>
2019-02-27 20:49:46 -0500

    final commit

--- london_fivestar_hotels_restaurants.sql
+++ london_fivestar_hotels_restaurants.sql
@@ -16,3 +16,29 @@ CREATE TABLE restaurant_final (
     Number_of_Reviews INT
     );
     
+USE london_fivestar;
+ALTER TABLE restaurant_final 
+	MODIFY COLUMN id INT(30) DEFAULT 0;
+    
+USE london_fivestar;
+ALTER TABLE hotel_final 
+	MODIFY COLUMN Property_Name VARCHAR(30) PRIMARY KEY;
+    
+USE london_fivestar;
+DROP TABLE london_fivestar;
+
+USE london_fivestar;
+ALTER TABLE hotel_final 
+	DROP id;
+    
+USE london_fivestar;
+ALTER TABLE hotel_final 
+	MODIFY COLUMN Property_Name VARCHAR(100), 
+    MODIFY COLUMN Review_Rating DECIMAL(2,1);
+    
+USE london_fivestar;
+	TRUNCATE restaurant_final;
+    TRUNCATE hotel_final;
+    
+
+
